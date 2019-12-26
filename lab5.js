d84472c1dd2b39e34a7169a8816d71f5a6183ceb
Imbaskin <58823125+Imbaskin@users.noreply.github.com>
2019-12-26 18:46:38 +0200

    jfkjfk

--- lab5.js
+++ lab5.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hi there!\n');
-});
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 80
+
+app.get('/',(req, res)=>{
+  res.end('Hi, There!')
+})
